e37cf751f998f9d8815b0c8845e186c131c37dd1
prateek <prateek@bedbathmore.com>
2016-12-03 01:53:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+"use strict"
+
+var fs = require('fs');
+var q = require('q');
+var http = require('http');
+var io = require('socket.io');
+var server = http.createServer()
+var socket = io.listen(server);
+var number = 1;
+var notCalled = true;
+var lines = [];
+var totallines = 0;
+
+function getData() {
+    var defer = q.defer();
+    fs.readFile(__dirname + '/filllist.txt', function(err, data) {
+        if (err) {
+            defer.reject('Error : -' + number);
+            return;
+        }
+        if (notCalled) {
+            lines = data.toString('utf-8').split("\n");
+            totallines = lines.length;
+            notCalled = false;
+        }
+        if (number >= totallines) {
+            return defer.reject(err);
+        }
+        defer.resolve(lines[number]);
+    });
+    return defer.promise;
+}
+
+function printData() {
+    var promise = getData();
+    promise.then(function(data) {
+        console.log(data);
+        number = number + 1;
+        socket.emit('broad', data);
+        setTimeout(printData, 1000);
+    }, function(err) {
+        number = 1;
+    });
+}
+
+
+socket.on('connection', function(socket) {
+    console.log('socket');
+});
+
+server.listen(3000, function() {
+    console.log('Listening at: http://localhost:3000');
+    printData();
+});
